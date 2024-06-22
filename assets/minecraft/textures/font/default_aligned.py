6e1de3930547ed9fe06a0615a6950ccb26d757d2
Gameboy612 <64597724+Gameboy612@users.noreply.github.com>
2024-06-23 00:31:42 +0800

    number font

---
+++ assets/minecraft/textures/font/default_aligned.py
@@ -0,0 +1,22 @@
+import os
+from PIL import Image
+
+
+def generateFiles(path, save_path, height, num_range = (0, 10)):
+    for i in range(num_range[0], num_range[1] + 1, 1):
+        image = Image.open(path.replace("$", str(i)))
+
+        extended_image = Image.new("RGBA", (image.size[0], height))
+
+        extended_image.paste(image, (0, height - image.size[1]))
+        
+        extended_image.save(save_path.replace("$", str(i)))
+
+for name in os.listdir("assets/minecraft/textures/font/default"):
+    if not name.endswith(".png"):
+        continue
+    generateFiles(
+        path=f"assets/minecraft/textures/font/default/{name}",
+        save_path=f"assets/minecraft/textures/font/default_aligned/{name}",
+        height=40
+    )
